3929a47daefe8c8b2d4e5f287f58ffda39f19cf8
novaxux <manuelova@outlook.es>
2025-09-30 12:40:52 -0600

    logout button added

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,22 @@
-const header = () => {
+import logoutIcon from "../assets/icons/logout.svg";
+const Header = () => {
+  const handleLogout = async () => {
+    try {
+      console.log("Sesión cerrada correctamente");
+      const logoutUrl = `${import.meta.env.VITE_AUTH}/logout.php`;
+      window.location.href = logoutUrl; 
+    } catch (error) {
+      console.error("Error al cerrar sesión:", error);
+    }
+  };
   return (
-    <header>
-      <span></span>
+    <header className="header">
       <h1>My To-Do List 🖉</h1>
+      <button className="logout-btn" onClick={handleLogout}>
+        <img src={logoutIcon} alt="logout" />
+      </button>
     </header>
   );
-}
+};
 
-export default header;
+export default Header;
