6939fb5e2108305005c9ba356fe9b5d49ae7ce91
novaxux <manuelova@outlook.es>
2025-09-30 12:42:42 -0600

    login removed using href instead

--- src/api/auth.js
+++ src/api/auth.js
@@ -4,9 +4,6 @@ class ApiAuth {
     static verify() {
         return apiAuth.get("/verify.php");
     }
-    static login() {
-        return apiAuth.get("/login.php");
-    }
 }
 
 export default ApiAuth;
